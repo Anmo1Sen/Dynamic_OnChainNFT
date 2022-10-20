3153e2e28098f78069fe8947f4dbbb6a34686916
Anmol Sen <105110161+Anmo1Sen@users.noreply.github.com>
2022-10-20 11:25:05 +0530

    Update ChainBattles.sol

--- contracts/ChainBattles.sol
+++ contracts/ChainBattles.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";  //helps json data to be converted into string.
+import "@openzeppelin/contracts/utils/Base64.sol";    // converts svg image into alphanumeric uri which can be used to retrive the image.
 
 contract ChainBattles is ERC721URIStorage  {
     using Strings for uint256;
@@ -30,12 +30,13 @@ contract ChainBattles is ERC721URIStorage  {
     return string(
         abi.encodePacked(
             "data:image/svg+xml;base64,",
-            Base64.encode(svg)
+            Base64.encode(svg)                          //here when we put svg in base64.encode and then put it after  "data:image/svg+xml;base64 it becomes a url 
+                                                        // we are using abi.encodePacked to convert data into byted and then string when encapsulated in string();
         )    
     );
 }
 function getLevels(uint256 tokenId) public view returns (string memory) {
-    uint256 levels = tokenIdToLevels[tokenId];
+    uint256 levels = tokenIdToLevels[tokenId];   //mapping levels to token id that is it gives the data structure of the levels for tokenid.
     return levels.toString();
 }
 function getTokenURI(uint256 tokenId) public view returns (string memory){
@@ -57,14 +58,14 @@ function mint() public {
     _tokenIds.increment();
     uint256 newItemId = _tokenIds.current();
     _safeMint(msg.sender, newItemId);
-    tokenIdToLevels[newItemId] = 0;
+    tokenIdToLevels[newItemId] = 0;      //we set the levels to zero so when ever the item is minted it is assigned level = 0 ; we can increment count by increasing count as we do so in train fn.
     _setTokenURI(newItemId, getTokenURI(newItemId));
 }
 function train(uint256 tokenId) public {
     require(_exists(tokenId), "Please use an existing token");
     require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
     uint256 currentLevel = tokenIdToLevels[tokenId];
-    tokenIdToLevels[tokenId] = currentLevel + 1;
+    tokenIdToLevels[tokenId] = currentLevel + 1;  // when  ever train fn is called it increase the level count and the updated level update the uri.
     _setTokenURI(tokenId, getTokenURI(tokenId));
 }
 }
